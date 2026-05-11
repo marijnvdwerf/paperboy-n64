17150b312660ddf01e3498a36f2a79622300f435
Marijn van der Werf <marijn.vanderwerf@gmail.com>
2026-05-11 12:01:59 +0200

    split 3DE40

---
+++ src/3DE40.cpp
@@ -0,0 +1,77 @@
+#include "common.h"
+
+INCLUDE_ASM("asm/nonmatchings/3DE40", vfunc1__12StructWWBase);
+
+INCLUDE_ASM("asm/nonmatchings/3DE40", vfunc2__12StructWWBase);
+
+INCLUDE_ASM("asm/nonmatchings/3DE40", func_8003D6AC);
+
+INCLUDE_ASM("asm/nonmatchings/3DE40", func_8003D76C);
+
+INCLUDE_ASM("asm/nonmatchings/3DE40", func_8003D778);
+
+INCLUDE_ASM("asm/nonmatchings/3DE40", vfunc13__12StructWWBase);
+
+INCLUDE_ASM("asm/nonmatchings/3DE40", vfunc4__12StructWWBase);
+
+INCLUDE_ASM("asm/nonmatchings/3DE40", vfunc3__12StructWWBase);
+
+INCLUDE_ASM("asm/nonmatchings/3DE40", vfunc10__12StructWWBase);
+
+INCLUDE_ASM("asm/nonmatchings/3DE40", vfunc9__12StructWWBase);
+
+INCLUDE_ASM("asm/nonmatchings/3DE40", vfunc8__12StructWWBase);
+
+INCLUDE_ASM("asm/nonmatchings/3DE40", vfunc7__12StructWWBase);
+
+INCLUDE_ASM("asm/nonmatchings/3DE40", vfunc6__12StructWWBaseP8StructVV);
+
+INCLUDE_ASM("asm/nonmatchings/3DE40", vfunc5__12StructWWBase);
+
+INCLUDE_ASM("asm/nonmatchings/3DE40", func_8003DBD4);
+
+INCLUDE_ASM("asm/nonmatchings/3DE40", _._12StructWWBase);
+
+INCLUDE_ASM("asm/nonmatchings/3DE40", __12StructWWBase);
+
+INCLUDE_ASM("asm/nonmatchings/3DE40", func_8003DCA4);
+
+INCLUDE_ASM("asm/nonmatchings/3DE40", func_8003DCC0);
+
+INCLUDE_ASM("asm/nonmatchings/3DE40", func_8003DCCC);
+
+INCLUDE_ASM("asm/nonmatchings/3DE40", func_8003DCD8);
+
+INCLUDE_ASM("asm/nonmatchings/3DE40", func_8003DCE0);
+
+INCLUDE_ASM("asm/nonmatchings/3DE40", func_8003DE30);
+
+INCLUDE_ASM("asm/nonmatchings/3DE40", func_8003DE3C);
+
+INCLUDE_ASM("asm/nonmatchings/3DE40", func_8003DE48);
+
+INCLUDE_ASM("asm/nonmatchings/3DE40", func_8003DE90);
+
+INCLUDE_ASM("asm/nonmatchings/3DE40", func_8003DED8);
+
+INCLUDE_ASM("asm/nonmatchings/3DE40", func_8003DEE4);
+
+INCLUDE_ASM("asm/nonmatchings/3DE40", func_8003DF70);
+
+INCLUDE_ASM("asm/nonmatchings/3DE40", func_8003E010);
+
+INCLUDE_ASM("asm/nonmatchings/3DE40", func_8003E084);
+
+INCLUDE_ASM("asm/nonmatchings/3DE40", func_8003E178);
+
+INCLUDE_ASM("asm/nonmatchings/3DE40", func_8003E1C0);
+
+INCLUDE_ASM("asm/nonmatchings/3DE40", func_8003E204);
+
+INCLUDE_ASM("asm/nonmatchings/3DE40", func_8003E210);
+
+INCLUDE_ASM("asm/nonmatchings/3DE40", func_8003E218);
+
+INCLUDE_ASM("asm/nonmatchings/3DE40", func_8003E220);
+
+INCLUDE_ASM("asm/nonmatchings/3DE40", func_8003E22C);
