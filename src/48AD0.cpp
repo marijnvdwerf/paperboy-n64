9644a0f131b329a11c347fa9eecf316d87d6c86f
Marijn van der Werf <marijn.vanderwerf@gmail.com>
2026-05-13 01:47:08 +0200

    base

---
+++ src/48AD0.cpp
@@ -0,0 +1,93 @@
+#include "common.h"
+
+INCLUDE_ASM("asm/nonmatchings/48AD0", func_80047ED0);
+
+INCLUDE_ASM("asm/nonmatchings/48AD0", func_80048024);
+
+extern "C" void func_80048250(void) {
+}
+
+extern "C" void func_80048258(void) {
+}
+
+extern "C" void func_80048260(void) {
+}
+
+extern "C" void func_80048268(void) {
+}
+
+extern "C" void func_80048270(void) {
+}
+
+extern "C" void func_80048278(void) {
+}
+
+INCLUDE_ASM("asm/nonmatchings/48AD0", func_80048280);
+
+INCLUDE_ASM("asm/nonmatchings/48AD0", func_80048288);
+
+INCLUDE_ASM("asm/nonmatchings/48AD0", func_80048290);
+
+INCLUDE_ASM("asm/nonmatchings/48AD0", func_80048298);
+
+INCLUDE_ASM("asm/nonmatchings/48AD0", func_800482A0);
+
+INCLUDE_ASM("asm/nonmatchings/48AD0", func_800482B4);
+
+INCLUDE_ASM("asm/nonmatchings/48AD0", func_8004836C);
+
+INCLUDE_ASM("asm/nonmatchings/48AD0", func_800483C4);
+
+INCLUDE_ASM("asm/nonmatchings/48AD0", func_80048414);
+
+INCLUDE_ASM("asm/nonmatchings/48AD0", func_80048464);
+
+INCLUDE_ASM("asm/nonmatchings/48AD0", func_800484A8);
+
+INCLUDE_ASM("asm/nonmatchings/48AD0", func_800484E0);
+
+INCLUDE_ASM("asm/nonmatchings/48AD0", func_80048610);
+
+INCLUDE_ASM("asm/nonmatchings/48AD0", func_80048648);
+
+INCLUDE_ASM("asm/nonmatchings/48AD0", func_8004869C);
+
+INCLUDE_RODATA("asm/nonmatchings/48AD0", D_80004E40);
+
+INCLUDE_ASM("asm/nonmatchings/48AD0", func_80048758);
+
+INCLUDE_ASM("asm/nonmatchings/48AD0", func_800487E4);
+
+INCLUDE_ASM("asm/nonmatchings/48AD0", func_8004880C);
+
+INCLUDE_ASM("asm/nonmatchings/48AD0", func_8004886C);
+
+INCLUDE_ASM("asm/nonmatchings/48AD0", func_80048924);
+
+INCLUDE_ASM("asm/nonmatchings/48AD0", func_8004896C);
+
+INCLUDE_ASM("asm/nonmatchings/48AD0", func_800489A8);
+
+INCLUDE_ASM("asm/nonmatchings/48AD0", func_800489B4);
+
+INCLUDE_ASM("asm/nonmatchings/48AD0", func_800489BC);
+
+INCLUDE_ASM("asm/nonmatchings/48AD0", func_800489C4);
+
+INCLUDE_ASM("asm/nonmatchings/48AD0", func_800489D0);
+
+INCLUDE_ASM("asm/nonmatchings/48AD0", func_800489DC);
+
+INCLUDE_ASM("asm/nonmatchings/48AD0", func_800489E8);
+
+INCLUDE_ASM("asm/nonmatchings/48AD0", func_800489F4);
+
+INCLUDE_ASM("asm/nonmatchings/48AD0", func_80048A00);
+
+INCLUDE_ASM("asm/nonmatchings/48AD0", func_80048A0C);
+
+INCLUDE_ASM("asm/nonmatchings/48AD0", func_80048A14);
+
+INCLUDE_ASM("asm/nonmatchings/48AD0", func_80048A1C);
+
+INCLUDE_RODATA("asm/nonmatchings/48AD0", D_80004F80);
