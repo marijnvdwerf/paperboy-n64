7b6a5166f957bf6403001436242861f87fc2c4db
Marijn van der Werf <marijn.vanderwerf@gmail.com>
2026-05-13 12:38:54 +0200

    Update yaml

--- src/C380.c
+++
@@ -1,8 +0,0 @@
-#include "common.h"
-
-INCLUDE_ASM("asm/nonmatchings/C380", func_8000B780);
-
-INCLUDE_ASM("asm/nonmatchings/C380", func_8000B7BC);
-
-GARBAGE_NTSC(0x8E02AE60);
-GARBAGE_NTSC(0x10400006);
