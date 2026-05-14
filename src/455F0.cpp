adb7d4120679f2cd8ce11e482204f3ecec95a1de
Marijn van der Werf <marijn.vanderwerf@gmail.com>
2026-05-14 04:32:44 +0200

    Migrate 455F0 to Cpp

---
+++ src/455F0.cpp
@@ -0,0 +1,47 @@
+#include "common.h"
+
+INCLUDE_ASM("asm/nonmatchings/455F0", func_800449F0);
+
+INCLUDE_ASM("asm/nonmatchings/455F0", func_80044BC8);
+
+INCLUDE_ASM("asm/nonmatchings/455F0", func_80045078);
+
+INCLUDE_ASM("asm/nonmatchings/455F0", func_80045114);
+
+INCLUDE_ASM("asm/nonmatchings/455F0", func_800451B0);
+
+INCLUDE_ASM("asm/nonmatchings/455F0", func_80045254);
+
+INCLUDE_ASM("asm/nonmatchings/455F0", func_8004527C);
+
+INCLUDE_ASM("asm/nonmatchings/455F0", func_80045288__8StructZZ);
+
+INCLUDE_ASM("asm/nonmatchings/455F0", func_800452A4);
+
+INCLUDE_ASM("asm/nonmatchings/455F0", func_800452D8);
+
+INCLUDE_ASM("asm/nonmatchings/455F0", func_800452E4);
+
+INCLUDE_ASM("asm/nonmatchings/455F0", func_800453F8);
+
+INCLUDE_ASM("asm/nonmatchings/455F0", func_8004553C);
+
+INCLUDE_RODATA("asm/nonmatchings/455F0", D_80004870);
+
+INCLUDE_RODATA("asm/nonmatchings/455F0", D_80004888);
+
+INCLUDE_ASM("asm/nonmatchings/455F0", func_8004556C);
+
+INCLUDE_ASM("asm/nonmatchings/455F0", _._8StructZZ);
+
+INCLUDE_ASM("asm/nonmatchings/455F0", __8StructZZ);
+
+INCLUDE_ASM("asm/nonmatchings/455F0", func_800456A8);
+
+INCLUDE_ASM("asm/nonmatchings/455F0", func_800456B4);
+
+INCLUDE_ASM("asm/nonmatchings/455F0", func_800456C0);
+
+INCLUDE_ASM("asm/nonmatchings/455F0", func_800456CC);
+
+INCLUDE_RODATA("asm/nonmatchings/455F0", D_80004898);
