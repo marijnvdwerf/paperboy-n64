b79ce9d42ebeca402d3d4c87d125367e58e15e72
Marijn van der Werf <marijn.vanderwerf@gmail.com>
2026-05-15 18:51:02 +0200

    otter

---
+++ src/otter.cpp
@@ -0,0 +1,37 @@
+#include "common.h"
+
+INCLUDE_ASM("asm/nonmatchings/otter", func_80049400);
+
+INCLUDE_ASM("asm/nonmatchings/otter", func_8004954C);
+
+INCLUDE_RODATA("asm/nonmatchings/otter", _vt.5Otter);
+
+INCLUDE_ASM("asm/nonmatchings/otter", func_800496DC);
+
+INCLUDE_ASM("asm/nonmatchings/otter", func_80049790);
+
+INCLUDE_ASM("asm/nonmatchings/otter", func_800498B8);
+
+INCLUDE_ASM("asm/nonmatchings/otter", func_80049980);
+
+INCLUDE_ASM("asm/nonmatchings/otter", func_80049A74);
+
+INCLUDE_ASM("asm/nonmatchings/otter", func_80049B50);
+
+INCLUDE_ASM("asm/nonmatchings/otter", func_80049B58);
+
+INCLUDE_ASM("asm/nonmatchings/otter", func_80049BC8);
+
+INCLUDE_ASM("asm/nonmatchings/otter", func_80049C24);
+
+INCLUDE_ASM("asm/nonmatchings/otter", func_80049CC4);
+
+INCLUDE_ASM("asm/nonmatchings/otter", _._5Otter);
+
+INCLUDE_ASM("asm/nonmatchings/otter", __5Otter);
+
+INCLUDE_ASM("asm/nonmatchings/otter", func_80049DA0);
+
+INCLUDE_ASM("asm/nonmatchings/otter", func_80049DAC);
+
+INCLUDE_ASM("asm/nonmatchings/otter", func_80049DB4);
