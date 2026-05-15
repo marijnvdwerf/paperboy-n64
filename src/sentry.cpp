50e75d73d5ed93745123c75cb9baa83f82569782
Marijn van der Werf <marijn.vanderwerf@gmail.com>
2026-05-15 17:45:38 +0200

    sentry

---
+++ src/sentry.cpp
@@ -0,0 +1,37 @@
+#include "common.h"
+
+INCLUDE_ASM("asm/nonmatchings/sentry", func_800456D0);
+
+INCLUDE_ASM("asm/nonmatchings/sentry", func_800456D8);
+
+INCLUDE_ASM("asm/nonmatchings/sentry", func_800456E0);
+
+INCLUDE_ASM("asm/nonmatchings/sentry", .L800456E8);
+
+INCLUDE_ASM("asm/nonmatchings/sentry", _._6Sentry);
+
+INCLUDE_ASM("asm/nonmatchings/sentry", __6Sentry);
+
+INCLUDE_ASM("asm/nonmatchings/sentry", func_8004573C);
+
+INCLUDE_ASM("asm/nonmatchings/sentry", func_80045744);
+
+INCLUDE_ASM("asm/nonmatchings/sentry", func_80045750);
+
+INCLUDE_ASM("asm/nonmatchings/sentry", func_8004575C);
+
+INCLUDE_ASM("asm/nonmatchings/sentry", func_80045768);
+
+INCLUDE_ASM("asm/nonmatchings/sentry", func_80045774);
+
+INCLUDE_ASM("asm/nonmatchings/sentry", func_80045780);
+
+INCLUDE_ASM("asm/nonmatchings/sentry", func_8004578C);
+
+INCLUDE_ASM("asm/nonmatchings/sentry", func_80045798);
+
+INCLUDE_ASM("asm/nonmatchings/sentry", func_800457AC);
+
+INCLUDE_ASM("asm/nonmatchings/sentry", func_800457B8);
+
+INCLUDE_RODATA("asm/nonmatchings/sentry", _vt.6Sentry);
