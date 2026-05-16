9cae432786868916207e400d9d1b8967973168c9
Marijn van der Werf <marijn.vanderwerf@gmail.com>
2026-05-16 16:37:55 +0200

    pixel_format

---
+++ src/pixel_format.cpp
@@ -0,0 +1,29 @@
+#include "common.h"
+
+INCLUDE_ASM("asm/nonmatchings/pixel_format", func_80015510);
+
+INCLUDE_ASM("asm/nonmatchings/pixel_format", func_80015730);
+
+INCLUDE_ASM("asm/nonmatchings/pixel_format", func_8001596C);
+
+INCLUDE_ASM("asm/nonmatchings/pixel_format", func_800159A8);
+
+INCLUDE_ASM("asm/nonmatchings/pixel_format", func_800159E0);
+
+INCLUDE_ASM("asm/nonmatchings/pixel_format", func_80015A18);
+
+INCLUDE_ASM("asm/nonmatchings/pixel_format", func_80015A50);
+
+INCLUDE_ASM("asm/nonmatchings/pixel_format", func_80015A88);
+
+INCLUDE_ASM("asm/nonmatchings/pixel_format", func_80015AC0);
+
+INCLUDE_ASM("asm/nonmatchings/pixel_format", func_80015B18);
+
+INCLUDE_ASM("asm/nonmatchings/pixel_format", func_80015B6C);
+
+INCLUDE_ASM("asm/nonmatchings/pixel_format", func_80015BC0);
+
+INCLUDE_ASM("asm/nonmatchings/pixel_format", func_80015C14);
+
+INCLUDE_ASM("asm/nonmatchings/pixel_format", func_80015C68);
