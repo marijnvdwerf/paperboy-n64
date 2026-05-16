d4de81dc08c5230b841e0a4269c8963f0ebd3a38
Marijn van der Werf <marijn.vanderwerf@gmail.com>
2026-05-16 07:54:50 +0200

    477C0

---
+++ src/477C0.cpp
@@ -0,0 +1,65 @@
+#include "common.h"
+
+INCLUDE_ASM("asm/nonmatchings/477C0", func_80046BC0);
+
+INCLUDE_ASM("asm/nonmatchings/477C0", func_80046D30);
+
+INCLUDE_ASM("asm/nonmatchings/477C0", func_80046E40);
+
+INCLUDE_ASM("asm/nonmatchings/477C0", func_80047398);
+
+INCLUDE_ASM("asm/nonmatchings/477C0", func_800473B8);
+
+INCLUDE_ASM("asm/nonmatchings/477C0", func_800473C4);
+
+INCLUDE_ASM("asm/nonmatchings/477C0", func_80047428);
+
+INCLUDE_ASM("asm/nonmatchings/477C0", func_80047490);
+
+INCLUDE_ASM("asm/nonmatchings/477C0", func_800474F8);
+
+INCLUDE_ASM("asm/nonmatchings/477C0", func_80047558);
+
+INCLUDE_ASM("asm/nonmatchings/477C0", func_800475B8);
+
+INCLUDE_ASM("asm/nonmatchings/477C0", func_80047618);
+
+INCLUDE_ASM("asm/nonmatchings/477C0", func_8004767C);
+
+INCLUDE_ASM("asm/nonmatchings/477C0", func_800476DC);
+
+INCLUDE_ASM("asm/nonmatchings/477C0", func_8004775C);
+
+INCLUDE_ASM("asm/nonmatchings/477C0", func_800477D4);
+
+INCLUDE_ASM("asm/nonmatchings/477C0", func_80047854);
+
+INCLUDE_ASM("asm/nonmatchings/477C0", func_800478DC);
+
+INCLUDE_ASM("asm/nonmatchings/477C0", func_8004798C);
+
+INCLUDE_ASM("asm/nonmatchings/477C0", func_800479F0);
+
+INCLUDE_ASM("asm/nonmatchings/477C0", func_80047A50);
+
+INCLUDE_ASM("asm/nonmatchings/477C0", func_80047A64);
+
+INCLUDE_ASM("asm/nonmatchings/477C0", func_80047A78);
+
+INCLUDE_ASM("asm/nonmatchings/477C0", func_80047A84);
+
+INCLUDE_ASM("asm/nonmatchings/477C0", func_80047B94);
+
+INCLUDE_ASM("asm/nonmatchings/477C0", func_80047C28);
+
+INCLUDE_ASM("asm/nonmatchings/477C0", func_80047CF4);
+
+INCLUDE_ASM("asm/nonmatchings/477C0", _._15StructYYSubD0E8);
+
+INCLUDE_ASM("asm/nonmatchings/477C0", __15StructYYSubD0E8);
+
+INCLUDE_ASM("asm/nonmatchings/477C0", func_80047E6C);
+
+INCLUDE_ASM("asm/nonmatchings/477C0", func_80047EC8);
+
+INCLUDE_RODATA("asm/nonmatchings/477C0", D_80004DC0);
