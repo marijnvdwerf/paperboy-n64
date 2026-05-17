545c85d18cef10989e2b21fa89da962eec7f961f
Marijn van der Werf <marijn.vanderwerf@gmail.com>
2026-05-17 15:07:55 +0200

    magpie

---
+++ src/magpie.cpp
@@ -0,0 +1,17 @@
+#include "common.h"
+
+INCLUDE_ASM("asm/nonmatchings/magpie", func_80049DC0);
+
+INCLUDE_ASM("asm/nonmatchings/magpie", func_80049F18);
+
+INCLUDE_ASM("asm/nonmatchings/magpie", func_8004A00C);
+
+INCLUDE_ASM("asm/nonmatchings/magpie", __6Magpie);
+
+INCLUDE_ASM("asm/nonmatchings/magpie", func_8004A0A4);
+
+INCLUDE_ASM("asm/nonmatchings/magpie", func_8004A0B0);
+
+INCLUDE_ASM("asm/nonmatchings/magpie", _._6Magpie);
+
+INCLUDE_RODATA("asm/nonmatchings/magpie", _vt.6Magpie);
