11ba289e7c8e8d79d5d2b4f371fd74ac1b33643d
Marijn van der Werf <marijn.vanderwerf@gmail.com>
2026-05-17 12:41:23 +0200

    kookaburra

---
+++ src/kookaburra.cpp
@@ -0,0 +1,35 @@
+#include "common.h"
+
+INCLUDE_ASM("asm/nonmatchings/kookaburra", func_800459A0);
+
+INCLUDE_ASM("asm/nonmatchings/kookaburra", func_80045C18);
+
+INCLUDE_ASM("asm/nonmatchings/kookaburra", func_80045C9C);
+
+INCLUDE_ASM("asm/nonmatchings/kookaburra", func_80045CE0);
+
+INCLUDE_ASM("asm/nonmatchings/kookaburra", func_80045D3C);
+
+INCLUDE_ASM("asm/nonmatchings/kookaburra", func_80045D8C);
+
+INCLUDE_ASM("asm/nonmatchings/kookaburra", func_80045DEC);
+
+INCLUDE_ASM("asm/nonmatchings/kookaburra", _._10Kookaburra);
+
+INCLUDE_ASM("asm/nonmatchings/kookaburra", __10Kookaburra);
+
+INCLUDE_ASM("asm/nonmatchings/kookaburra", func_80045ED0);
+
+INCLUDE_ASM("asm/nonmatchings/kookaburra", func_80045EDC);
+
+INCLUDE_ASM("asm/nonmatchings/kookaburra", func_80045EE8);
+
+INCLUDE_ASM("asm/nonmatchings/kookaburra", func_80045F0C);
+
+INCLUDE_ASM("asm/nonmatchings/kookaburra", _._9Frogmouth);
+
+INCLUDE_RODATA("asm/nonmatchings/kookaburra", D_800049C0);
+
+INCLUDE_RODATA("asm/nonmatchings/kookaburra", _vt.10Kookaburra);
+
+INCLUDE_RODATA("asm/nonmatchings/kookaburra", _vt.9Frogmouth);
