357bc58ca6be8d00f2cf7b065b8fef1641c6d23c
Marijn van der Werf <marijn.vanderwerf@gmail.com>
2026-05-17 14:28:45 +0200

    sandpiper

---
+++ src/sandpiper.cpp
@@ -0,0 +1,13 @@
+#include "common.h"
+
+INCLUDE_ASM("asm/nonmatchings/sandpiper", func_80010C90);
+
+INCLUDE_ASM("asm/nonmatchings/sandpiper", func_80010E74);
+
+INCLUDE_ASM("asm/nonmatchings/sandpiper", func_80010E90);
+
+INCLUDE_ASM("asm/nonmatchings/sandpiper", func_80010EAC);
+
+INCLUDE_ASM("asm/nonmatchings/sandpiper", func_80010EF0);
+
+INCLUDE_RODATA("asm/nonmatchings/sandpiper", D_80000ED4);
