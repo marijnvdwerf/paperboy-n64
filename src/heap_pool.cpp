75c48b19a8bbaae6a6ef8166861c0e5958074c17
Marijn van der Werf <marijn.vanderwerf@gmail.com>
2026-05-18 22:14:27 +0200

    heap pool

---
+++ src/heap_pool.cpp
@@ -0,0 +1,27 @@
+#include "common.h"
+
+INCLUDE_ASM("asm/nonmatchings/heap_pool", func_8004ABD0);
+
+INCLUDE_ASM("asm/nonmatchings/heap_pool", func_8004AD70);
+
+INCLUDE_ASM("asm/nonmatchings/heap_pool", func_8004AE98);
+
+INCLUDE_ASM("asm/nonmatchings/heap_pool", func_8004AECC);
+
+INCLUDE_ASM("asm/nonmatchings/heap_pool", func_8004AEE8);
+
+INCLUDE_ASM("asm/nonmatchings/heap_pool", func_8004AF5C);
+
+INCLUDE_ASM("asm/nonmatchings/heap_pool", func_8004AFE4);
+
+INCLUDE_ASM("asm/nonmatchings/heap_pool", func_8004B0D4);
+
+INCLUDE_ASM("asm/nonmatchings/heap_pool", func_8004B100);
+
+INCLUDE_ASM("asm/nonmatchings/heap_pool", func_8004B1EC);
+
+INCLUDE_ASM("asm/nonmatchings/heap_pool", func_8004B268);
+
+INCLUDE_ASM("asm/nonmatchings/heap_pool", __8HeapPool);
+
+INCLUDE_RODATA("asm/nonmatchings/heap_pool", D_80005410);
