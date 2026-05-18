f36fafc0168043459256e8f2d173c910f4a4de5f
Marijn van der Werf <marijn.vanderwerf@gmail.com>
2026-05-18 13:09:35 +0200

    penguin

---
+++ src/penguin.cpp
@@ -0,0 +1,19 @@
+#include "common.h"
+
+INCLUDE_ASM("asm/nonmatchings/penguin", func_80048E80);
+
+INCLUDE_ASM("asm/nonmatchings/penguin", func_80048FC4);
+
+INCLUDE_ASM("asm/nonmatchings/penguin", func_80048FD0);
+
+INCLUDE_ASM("asm/nonmatchings/penguin", func_80048FD8);
+
+INCLUDE_ASM("asm/nonmatchings/penguin", func_80049048);
+
+INCLUDE_ASM("asm/nonmatchings/penguin", func_800490E0);
+
+INCLUDE_ASM("asm/nonmatchings/penguin", __7Penguin);
+
+INCLUDE_ASM("asm/nonmatchings/penguin", _._7Penguin);
+
+INCLUDE_RODATA("asm/nonmatchings/penguin", _vt.7Penguin);
