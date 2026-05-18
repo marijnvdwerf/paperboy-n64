923c9706f0b8398022754029c94124ba525c5fda
Marijn van der Werf <marijn.vanderwerf@gmail.com>
2026-05-18 20:07:26 +0200

    heap

---
+++ src/heap.cpp
@@ -0,0 +1,17 @@
+#include "common.h"
+
+INCLUDE_ASM("asm/nonmatchings/heap", func_8004A6F0);
+
+INCLUDE_ASM("asm/nonmatchings/heap", func_8004A8C4);
+
+INCLUDE_ASM("asm/nonmatchings/heap", func_8004AA78);
+
+INCLUDE_ASM("asm/nonmatchings/heap", func_8004AA98);
+
+INCLUDE_ASM("asm/nonmatchings/heap", func_8004AAD4);
+
+INCLUDE_ASM("asm/nonmatchings/heap", func_8004AAF0);
+
+INCLUDE_ASM("asm/nonmatchings/heap", func_8004AB78);
+
+INCLUDE_ASM("asm/nonmatchings/heap", func_8004ABA4);
