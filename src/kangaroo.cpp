b6d1ad7692d702181d9cec06c4daa6bc0957eeb5
Marijn van der Werf <marijn.vanderwerf@gmail.com>
2026-05-20 14:09:11 +0200

    kangaroo

---
+++ src/kangaroo.cpp
@@ -0,0 +1,79 @@
+#include "common.h"
+
+INCLUDE_ASM("asm/nonmatchings/kangaroo", func_8002ADC0);
+
+INCLUDE_ASM("asm/nonmatchings/kangaroo", func_8002AF2C);
+
+INCLUDE_ASM("asm/nonmatchings/kangaroo", func_8002AF90);
+
+INCLUDE_ASM("asm/nonmatchings/kangaroo", func_8002AFF4);
+
+INCLUDE_ASM("asm/nonmatchings/kangaroo", func_8002B010);
+
+INCLUDE_ASM("asm/nonmatchings/kangaroo", func_8002B02C);
+
+INCLUDE_ASM("asm/nonmatchings/kangaroo", func_8002B068);
+
+INCLUDE_ASM("asm/nonmatchings/kangaroo", func_8002B084);
+
+INCLUDE_ASM("asm/nonmatchings/kangaroo", func_8002B0A0);
+
+INCLUDE_RODATA("asm/nonmatchings/kangaroo", _vt.8Kangaroo);
+
+INCLUDE_ASM("asm/nonmatchings/kangaroo", func_8002B0BC);
+
+INCLUDE_ASM("asm/nonmatchings/kangaroo", func_8002B1D0);
+
+INCLUDE_ASM("asm/nonmatchings/kangaroo", func_8002B2CC);
+
+INCLUDE_ASM("asm/nonmatchings/kangaroo", func_8002B3B0);
+
+INCLUDE_ASM("asm/nonmatchings/kangaroo", func_8002B408);
+
+INCLUDE_ASM("asm/nonmatchings/kangaroo", func_8002B4CC);
+
+INCLUDE_ASM("asm/nonmatchings/kangaroo", func_8002B590);
+
+INCLUDE_ASM("asm/nonmatchings/kangaroo", func_8002B684);
+
+INCLUDE_ASM("asm/nonmatchings/kangaroo", func_8002B7C8);
+
+INCLUDE_ASM("asm/nonmatchings/kangaroo", func_8002B8FC);
+
+INCLUDE_ASM("asm/nonmatchings/kangaroo", func_8002B930);
+
+INCLUDE_ASM("asm/nonmatchings/kangaroo", func_8002B964);
+
+INCLUDE_ASM("asm/nonmatchings/kangaroo", __8Kangaroo);
+
+INCLUDE_ASM("asm/nonmatchings/kangaroo", func_8002BA6C);
+
+INCLUDE_ASM("asm/nonmatchings/kangaroo", func_8002BA88);
+
+INCLUDE_ASM("asm/nonmatchings/kangaroo", func_8002BA90);
+
+INCLUDE_ASM("asm/nonmatchings/kangaroo", func_8002BA98);
+
+INCLUDE_ASM("asm/nonmatchings/kangaroo", func_8002BAF4);
+
+INCLUDE_ASM("asm/nonmatchings/kangaroo", func_8002BB50);
+
+INCLUDE_ASM("asm/nonmatchings/kangaroo", func_8002BBDC);
+
+INCLUDE_ASM("asm/nonmatchings/kangaroo", func_8002BC14);
+
+INCLUDE_ASM("asm/nonmatchings/kangaroo", func_8002BC28);
+
+INCLUDE_ASM("asm/nonmatchings/kangaroo", func_8002BC6C);
+
+INCLUDE_ASM("asm/nonmatchings/kangaroo", func_8002BCAC);
+
+INCLUDE_ASM("asm/nonmatchings/kangaroo", func_8002BD30);
+
+INCLUDE_ASM("asm/nonmatchings/kangaroo", _._9Bowerbird);
+
+INCLUDE_ASM("asm/nonmatchings/kangaroo", __9Bowerbird);
+
+INCLUDE_RODATA("asm/nonmatchings/kangaroo", D_800028F8);
+
+INCLUDE_RODATA("asm/nonmatchings/kangaroo", D_80002900);
