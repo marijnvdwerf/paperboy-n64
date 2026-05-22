f82f8c5597ebf2d93c569aed6a9f60c69f10b6ed
Marijn van der Werf <marijn.vanderwerf@gmail.com>
2026-05-22 19:47:14 +0200

    bunyip

---
+++ src/bunyip.cpp
@@ -0,0 +1,49 @@
+#include "common.h"
+
+INCLUDE_ASM("asm/nonmatchings/bunyip", func_80028F80);
+
+INCLUDE_ASM("asm/nonmatchings/bunyip", __6Bunyip);
+
+INCLUDE_ASM("asm/nonmatchings/bunyip", func_80029194);
+
+INCLUDE_ASM("asm/nonmatchings/bunyip", func_8002919C);
+
+INCLUDE_ASM("asm/nonmatchings/bunyip", func_800291A8);
+
+INCLUDE_ASM("asm/nonmatchings/bunyip", func_800291B0);
+
+INCLUDE_ASM("asm/nonmatchings/bunyip", func_800291D8);
+
+INCLUDE_ASM("asm/nonmatchings/bunyip", func_80029200);
+
+INCLUDE_ASM("asm/nonmatchings/bunyip", func_80029218);
+
+INCLUDE_ASM("asm/nonmatchings/bunyip", func_8002923C);
+
+INCLUDE_ASM("asm/nonmatchings/bunyip", func_80029248);
+
+INCLUDE_ASM("asm/nonmatchings/bunyip", func_80029254);
+
+INCLUDE_ASM("asm/nonmatchings/bunyip", func_80029260);
+
+INCLUDE_ASM("asm/nonmatchings/bunyip", func_8002926C);
+
+INCLUDE_ASM("asm/nonmatchings/bunyip", func_80029278);
+
+INCLUDE_ASM("asm/nonmatchings/bunyip", func_80029284);
+
+INCLUDE_ASM("asm/nonmatchings/bunyip", func_80029290);
+
+INCLUDE_ASM("asm/nonmatchings/bunyip", func_8002929C);
+
+INCLUDE_ASM("asm/nonmatchings/bunyip", func_800292A8);
+
+INCLUDE_ASM("asm/nonmatchings/bunyip", _._6Bunyip);
+
+INCLUDE_ASM("asm/nonmatchings/bunyip", func_800292D0);
+
+INCLUDE_ASM("asm/nonmatchings/bunyip", func_80029480);
+
+INCLUDE_RODATA("asm/nonmatchings/bunyip", D_80002430);
+
+INCLUDE_RODATA("asm/nonmatchings/bunyip", _vt.6Bunyip);
