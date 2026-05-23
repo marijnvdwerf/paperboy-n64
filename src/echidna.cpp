6c48942ae2f0943d128079c7a4f43c15f7d86c96
Marijn van der Werf <marijn.vanderwerf@gmail.com>
2026-05-23 10:38:09 +0200

    echidna

---
+++ src/echidna.cpp
@@ -0,0 +1,17 @@
+#include "common.h"
+
+INCLUDE_ASM("asm/nonmatchings/echidna", func_8003A340);
+
+INCLUDE_ASM("asm/nonmatchings/echidna", func_8003A34C);
+
+INCLUDE_ASM("asm/nonmatchings/echidna", func_8003A354);
+
+INCLUDE_ASM("asm/nonmatchings/echidna", func_8003A398);
+
+INCLUDE_ASM("asm/nonmatchings/echidna", _._7Echidna);
+
+INCLUDE_ASM("asm/nonmatchings/echidna", func_8003A508);
+
+INCLUDE_RODATA("asm/nonmatchings/echidna", D_800037A0);
+
+INCLUDE_RODATA("asm/nonmatchings/echidna", _vt.7Echidna);
