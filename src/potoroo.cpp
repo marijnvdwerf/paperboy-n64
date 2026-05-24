ee433ef2e8a9430dab3ca5837f0e69adbf332044
Marijn van der Werf <marijn.vanderwerf@gmail.com>
2026-05-24 09:52:32 +0200

    Potoroo

---
+++ src/potoroo.cpp
@@ -0,0 +1,53 @@
+#include "common.h"
+
+INCLUDE_ASM("asm/nonmatchings/potoroo", func_80029520);
+
+INCLUDE_ASM("asm/nonmatchings/potoroo", func_80029654);
+
+extern "C" void func_80029660(void) {
+}
+
+extern "C" void func_80029668(void) {
+}
+
+extern "C" void func_80029670(void) {
+}
+
+extern "C" void func_80029678(void) {
+}
+
+extern "C" void func_80029680(void) {
+}
+
+extern "C" void func_80029688(void) {
+}
+
+INCLUDE_ASM("asm/nonmatchings/potoroo", func_80029690);
+
+INCLUDE_ASM("asm/nonmatchings/potoroo", func_800296D0);
+
+INCLUDE_ASM("asm/nonmatchings/potoroo", func_800296E4);
+
+INCLUDE_ASM("asm/nonmatchings/potoroo", func_800296F4);
+
+INCLUDE_ASM("asm/nonmatchings/potoroo", func_80029700);
+
+INCLUDE_ASM("asm/nonmatchings/potoroo", func_80029740);
+
+INCLUDE_ASM("asm/nonmatchings/potoroo", func_80029778);
+
+INCLUDE_ASM("asm/nonmatchings/potoroo", _._7Potoroo);
+
+INCLUDE_ASM("asm/nonmatchings/potoroo", __7Potoroo);
+
+INCLUDE_ASM("asm/nonmatchings/potoroo", func_8002986C);
+
+INCLUDE_ASM("asm/nonmatchings/potoroo", func_80029878);
+
+INCLUDE_ASM("asm/nonmatchings/potoroo", func_80029884);
+
+INCLUDE_ASM("asm/nonmatchings/potoroo", func_80029890);
+
+INCLUDE_RODATA("asm/nonmatchings/potoroo", D_800024C0);
+
+INCLUDE_RODATA("asm/nonmatchings/potoroo", _vt.7Potoroo);
