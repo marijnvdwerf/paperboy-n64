3b121836300890fa46e2b5e99f33cca0983664a1
Marijn van der Werf <marijn.vanderwerf@gmail.com>
2026-05-24 10:38:06 +0200

    marsupial

---
+++ src/marsupial.cpp
@@ -0,0 +1,23 @@
+#include "common.h"
+
+INCLUDE_ASM("asm/nonmatchings/marsupial", func_8003AFA0);
+
+INCLUDE_ASM("asm/nonmatchings/marsupial", func_8003B144);
+
+INCLUDE_ASM("asm/nonmatchings/marsupial", func_8003B1B4);
+
+INCLUDE_ASM("asm/nonmatchings/marsupial", func_8003B1C0);
+
+INCLUDE_ASM("asm/nonmatchings/marsupial", _._9Marsupial);
+
+INCLUDE_ASM("asm/nonmatchings/marsupial", __9Marsupial);
+
+INCLUDE_ASM("asm/nonmatchings/marsupial", func_8003B284);
+
+INCLUDE_ASM("asm/nonmatchings/marsupial", func_8003B294);
+
+INCLUDE_ASM("asm/nonmatchings/marsupial", func_8003B2A0);
+
+INCLUDE_RODATA("asm/nonmatchings/marsupial", D_80003970);
+
+INCLUDE_RODATA("asm/nonmatchings/marsupial", _vt.9Marsupial);
