2627f3fab82784d3d9d4e9b9e4b9b319ab4a0e60
Marijn van der Werf <marijn.vanderwerf@gmail.com>
2026-05-25 13:23:22 +0200

    dingo

---
+++ src/dingo.cpp
@@ -0,0 +1,70 @@
+#include "common.h"
+
+INCLUDE_ASM("asm/nonmatchings/dingo", func_801221F0);
+
+extern "C" void func_801221F8(void) {
+}
+
+extern "C" void func_80122200(void) {
+}
+
+extern "C" void func_80122208(void) {
+}
+
+INCLUDE_RODATA("asm/nonmatchings/dingo", _vt.5Dingo);
+
+INCLUDE_ASM("asm/nonmatchings/dingo", func_80122210);
+
+INCLUDE_ASM("asm/nonmatchings/dingo", func_80122240);
+
+extern "C" void func_80122264(void) {
+}
+
+INCLUDE_ASM("asm/nonmatchings/dingo", func_8012226C);
+
+INCLUDE_ASM("asm/nonmatchings/dingo", func_80122288);
+
+INCLUDE_ASM("asm/nonmatchings/dingo", func_801222A4);
+
+INCLUDE_ASM("asm/nonmatchings/dingo", func_801222D8);
+
+extern "C" void func_8012230C(void) {
+}
+
+INCLUDE_ASM("asm/nonmatchings/dingo", func_80122314);
+
+INCLUDE_ASM("asm/nonmatchings/dingo", func_80122330);
+
+INCLUDE_ASM("asm/nonmatchings/dingo", func_80122354);
+
+INCLUDE_ASM("asm/nonmatchings/dingo", func_80122390);
+
+INCLUDE_ASM("asm/nonmatchings/dingo", func_801223AC);
+
+INCLUDE_ASM("asm/nonmatchings/dingo", func_801223FC);
+
+INCLUDE_ASM("asm/nonmatchings/dingo", func_80122414);
+
+INCLUDE_ASM("asm/nonmatchings/dingo", func_80122480);
+
+INCLUDE_ASM("asm/nonmatchings/dingo", func_8012249C);
+
+INCLUDE_ASM("asm/nonmatchings/dingo", func_801224B8);
+
+INCLUDE_ASM("asm/nonmatchings/dingo", func_801225B0);
+
+INCLUDE_ASM("asm/nonmatchings/dingo", func_8012263C);
+
+INCLUDE_ASM("asm/nonmatchings/dingo", func_801226B4);
+
+INCLUDE_ASM("asm/nonmatchings/dingo", func_8012277C);
+
+INCLUDE_ASM("asm/nonmatchings/dingo", __5Dingo);
+
+INCLUDE_ASM("asm/nonmatchings/dingo", func_8012288C);
+
+INCLUDE_ASM("asm/nonmatchings/dingo", func_801228DC);
+
+INCLUDE_ASM("asm/nonmatchings/dingo", func_8012292C);
+
+INCLUDE_ASM("asm/nonmatchings/dingo", func_80122948);
