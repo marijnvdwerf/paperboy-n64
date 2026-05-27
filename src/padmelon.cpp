2e7ce01541ac7a0c675b0f60883a017b069ebf6e
Marijn van der Werf <marijn.vanderwerf@gmail.com>
2026-05-28 00:00:08 +0200

    padmelon

---
+++ src/padmelon.cpp
@@ -0,0 +1,65 @@
+#include "common.h"
+
+INCLUDE_ASM("asm/nonmatchings/padmelon", func_80021920);
+
+INCLUDE_ASM("asm/nonmatchings/padmelon", func_80021A74);
+
+INCLUDE_ASM("asm/nonmatchings/padmelon", func_80021BC8);
+
+INCLUDE_ASM("asm/nonmatchings/padmelon", func_80021D1C);
+
+INCLUDE_ASM("asm/nonmatchings/padmelon", func_80021E88);
+
+INCLUDE_ASM("asm/nonmatchings/padmelon", func_8002205C);
+
+INCLUDE_ASM("asm/nonmatchings/padmelon", func_800220D0);
+
+INCLUDE_ASM("asm/nonmatchings/padmelon", func_80022144);
+
+INCLUDE_ASM("asm/nonmatchings/padmelon", func_80022168);
+
+INCLUDE_RODATA("asm/nonmatchings/padmelon", _vt.9Pademelon);
+
+INCLUDE_ASM("asm/nonmatchings/padmelon", func_800221A4);
+
+INCLUDE_ASM("asm/nonmatchings/padmelon", func_800221CC);
+
+INCLUDE_ASM("asm/nonmatchings/padmelon", func_800221E8);
+
+INCLUDE_ASM("asm/nonmatchings/padmelon", func_800222DC);
+
+INCLUDE_ASM("asm/nonmatchings/padmelon", func_800223A0);
+
+INCLUDE_ASM("asm/nonmatchings/padmelon", func_80022464);
+
+INCLUDE_ASM("asm/nonmatchings/padmelon", func_800224B0);
+
+INCLUDE_ASM("asm/nonmatchings/padmelon", func_800224FC);
+
+INCLUDE_ASM("asm/nonmatchings/padmelon", func_80022548);
+
+INCLUDE_ASM("asm/nonmatchings/padmelon", __9Pademelon);
+
+INCLUDE_ASM("asm/nonmatchings/padmelon", func_800225CC);
+
+INCLUDE_ASM("asm/nonmatchings/padmelon", func_80022618);
+
+INCLUDE_ASM("asm/nonmatchings/padmelon", func_8002264C);
+
+INCLUDE_ASM("asm/nonmatchings/padmelon", func_80022670);
+
+INCLUDE_ASM("asm/nonmatchings/padmelon", func_800226D4);
+
+INCLUDE_ASM("asm/nonmatchings/padmelon", func_800226F0);
+
+INCLUDE_ASM("asm/nonmatchings/padmelon", func_8002270C);
+
+INCLUDE_ASM("asm/nonmatchings/padmelon", func_80022728);
+
+INCLUDE_ASM("asm/nonmatchings/padmelon", func_80022744);
+
+INCLUDE_ASM("asm/nonmatchings/padmelon", func_80022760);
+
+INCLUDE_ASM("asm/nonmatchings/padmelon", func_800227D0);
+
+INCLUDE_ASM("asm/nonmatchings/padmelon", func_8002281C);
