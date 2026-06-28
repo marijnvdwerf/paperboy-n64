d8815d739a828f4d2ac195d9fd790052ae1a9302
Marijn van der Werf <marijn.vanderwerf@gmail.com>
2026-06-28 09:05:22 +0200

    currawong

---
+++ src/currawong.cpp
@@ -0,0 +1,21 @@
+#include "common.h"
+
+INCLUDE_ASM("asm/nonmatchings/currawong", func_8003AB20);
+
+INCLUDE_ASM("asm/nonmatchings/currawong", func_8003AD6C);
+
+INCLUDE_ASM("asm/nonmatchings/currawong", func_8003ADAC);
+
+INCLUDE_ASM("asm/nonmatchings/currawong", func_8003ADDC);
+
+INCLUDE_ASM("asm/nonmatchings/currawong", func_8003AE0C);
+
+INCLUDE_ASM("asm/nonmatchings/currawong", func_8003AE64);
+
+INCLUDE_ASM("asm/nonmatchings/currawong", __9Currawong);
+
+INCLUDE_ASM("asm/nonmatchings/currawong", _._9Currawong);
+
+INCLUDE_RODATA("asm/nonmatchings/currawong", D_800038E8);
+
+INCLUDE_RODATA("asm/nonmatchings/currawong", _vt.9Currawong);
